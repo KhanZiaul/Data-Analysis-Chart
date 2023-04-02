072075f9948f8a74ffea6bfec27cb9eadccc30e9
KHAN ZIAUL HASAN <khanzia16phy102@gmail.com>
2023-04-02 16:03:44 +0600

    mebu bar done for small device

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -6,14 +6,15 @@ const Navigation = () => {
     console.log(menu)
     return (
         <>
-            <div onClick={() => setMenu(!menu)}>
+            <div onClick={() => setMenu(!menu)} className='md:hidden'>
 
                 {
                    menu ?   <Bars3Icon className="h-6 w-6 text-purple-500 cursor-pointer" /> :  <XMarkIcon  className="h-6 w-6 text-purple-500 cursor-pointer"></XMarkIcon>
                 }
-               
+
             </div>
-            <ul className='text-white flex flex-col md:flex-row justify-between p-5 bg-sky-700'>
+
+            <ul className={`text-white flex flex-col md:flex-row justify-between p-5 bg-sky-700 absolute duration-500 md:static ${menu ? 'top-14' : '-top-48'}`}>
                 <a href="/home">Home</a>
                 <a href="/options">Options</a>
                 <a href="/feedback">Feedback</a>
