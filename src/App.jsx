aed5f52cf708d594ff7a0fcc609992030b798caa
KHAN ZIAUL HASAN <khanzia16phy102@gmail.com>
2023-04-02 23:12:58 +0600

    fetch data by axios done

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from './components/Header/Header'
 import DashBoard from './components/DashBoard/DashBoard';
+import Axios from './components/Axios/Axios';
 const App = () => {
   return (
     <div>
       <Header></Header>
       <DashBoard></DashBoard>
+      <Axios></Axios>
     </div>
   );
 };
--- src/components/Axios/Axios.jsx
+++ src/components/Axios/Axios.jsx
@@ -1,6 +1,13 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 const Axios = () => {
+    const [phoneData,setPhoneData] = useState([])
+    useEffect(()=> {
+        axios.get('https://openapi.programming-hero.com/api/phones?search=iphone')
+        .then(data => console.log(data.data.data))
+    },[])
+
     return (
         <div>
             
